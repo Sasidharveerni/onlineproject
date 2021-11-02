ebedc49366a8099567fe97cfb25fad2ed8dca5b8
Sasidharveerni <81133052+Sasidharveerni@users.noreply.github.com>
2021-11-02 09:28:56 +0530

    Add files via upload

--- developer.php
+++ developer.php
@@ -10,40 +10,20 @@ class Project extends SQLite3
     $this->open('./includes/project.db');
   }
 }
-/**
- *
- */
-class Login extends SQLite3
-{
-
-  function __construct()
-  {
-    $this->open('./includes/Login.db');
-  }
-}
-
-$db1=new Project();
-$db=new Login();
-$usr=$_COOKIE['OPMS'];
-$sql="select PROJECT from client where EMAIL='".$usr."'";
+$db=new Project();
+$sql="select PNAME from project";
 $res=$db->query($sql);
  ?>
 <html>
 <head>
-  <title>Client</title>
-  <link rel="stylesheet" type="text/css" href="./css/client.css">
+  <title>Project Manager</title>
+  <link rel="stylesheet" type="text/css" href="./css/developer.css">
   <link rel="stylesheet" type="text/css" href="./css/makeup3.css">
   <link rel="stylesheet" type="text/css" href="./css/header.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script>
-  function report()
-  {
-    window.open('http://localhost:1516/report.php','Report');
-  }
-  </script>
 </head>
 <body>
   <header>
@@ -65,34 +45,44 @@ $res=$db->query($sql);
   </nav>
 </header>
 <div class="main">
-  <h3>Projects</h3>
-  <table name="project" width=80%>
-    <tr>
-      <th>Project Name</th>
-      <th>Status</th>
-      <th>Report</th>
-    </tr>
-    <?php
-        while($row=$res->fetchArray(SQLITE3_ASSOC)){
-          $pro=$row['PROJECT'];
-          $sql="select STATUS from project where PNAME='".$pro."'";
-          $res1=$db1->query($sql);
-          $row1=$res1->fetchArray(SQLITE3_ASSOC);
+  <h3>Project Status</h3>
+  <form action="./includes/dev-inc.php" method="post">
+  <div class="one">
+    <select name="project" required>
+      <option value="" disabled selected>Select Project</option>
+      <?php
+           while($row=$res->fetchArray(SQLITE3_ASSOC)){
+       ?>
+       <option value="<?php echo $row['PNAME'];?>"><?php echo $row['PNAME']; ?></option>
+       <?php
+     }
+     $db->close()
      ?>
-    <tr>
-      <td><?php echo $pro;?></td>
-      <td><?php echo $row1['STATUS'];?></td>
-      <td><a href="javascript:void(0);" onclick="report()">Report</a></td>
-    </tr>
-    <?php
-  }
-  ?>
-  </table>
+    </select>
+  </div>
+  <div class="two">
+    <select name="stat">
+      <option value="" disabled selected>Update status</option>
+      <option value="10%">below 20%</option>
+      <option value="25%">below 30%</option>
+	    <option value="35%">below 40%</option>
+      <option value="45%">below 50%</option>
+	    <option value="55%">below 60%</option>
+      <option value="65%">below 70%</option>
+	    <option value="75%">below 80%</option>
+      <option value="85%">below 90%</option>
+	    <option value="100%">completed</option>
+
+    </select>
+  </div>
+  <button type="submit" name="update">Update</button>
+</form>
 </div>
 </body>
 </html>
 <?php
-if (!isset($_COOKIE['OPMS'])) {
+if(!isset($_COOKIE['OPMS'])){
+  $db->close();
   header("Location: http://localhost:1516");
 }
  ?>
